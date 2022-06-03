3aeb186e70af1f94665dea749b104d61a3dfb6cf
Celil Uysal <celiluysl15@gmail.com>
2022-06-03 23:19:34 +0300

    SplashActivity - created splash screen

---
+++ app/src/main/java/com/celiluysal/itunesexplorer/ui/splash/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.celiluysal.itunesexplorer.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.celiluysal.itunesexplorer.databinding.ActivitySplashBinding
+import com.celiluysal.itunesexplorer.ui.base.BaseActivity
+import com.celiluysal.itunesexplorer.ui.home.HomeActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BaseActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun loadUI() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
