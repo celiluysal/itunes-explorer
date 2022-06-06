5c4598d38792963e1cf544e6a5704ecd861db48d
Celil Uysal <celiluysl15@gmail.com>
2022-06-07 00:49:33 +0300

    cleanup

--- app/src/main/java/com/celiluysal/itunesexplorer/ui/home/search/adapter/MediaItemsViewHolder.kt
+++ app/src/main/java/com/celiluysal/itunesexplorer/ui/home/search/adapter/MediaItemsViewHolder.kt
@@ -1,17 +1,14 @@
 package com.celiluysal.itunesexplorer.ui.home.search.adapter
 
 import android.content.Context
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.celiluysal.itunesexplorer.R
 import com.celiluysal.itunesexplorer.data.model.responses.MediaItem
 import com.celiluysal.itunesexplorer.databinding.ItemMediaItemBinding
 import com.celiluysal.itunesexplorer.extensions.*
 import com.celiluysal.itunesexplorer.ui.base.listeners.RecyclerViewListener
-import dagger.hilt.android.qualifiers.ApplicationContext
 
 class MediaItemsViewHolder(
-    @ApplicationContext private val context: Context,
+    private val context: Context,
     private val binding: ItemMediaItemBinding
 ) :
     RecyclerView.ViewHolder(binding.root) {
@@ -25,7 +22,8 @@ class MediaItemsViewHolder(
                 priceTextview.setPriceText(context, existingPrice, currency)
 
                 root.setOnClickListener {
-                    listener.onItemClicked(absoluteAdapterPosition)
+                    listener.onItemClicked(
+                        adapterPosition)
                 }
             }
         }
