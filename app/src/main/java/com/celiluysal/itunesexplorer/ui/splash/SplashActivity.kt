d464aacfc7c2222b4af9ea88656b92a5a83602a5
Celil Uysal <celiluysl15@gmail.com>
2022-06-08 01:21:45 +0300

    SplashViewModel - added check internet connection

--- app/src/main/java/com/celiluysal/itunesexplorer/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/celiluysal/itunesexplorer/ui/splash/SplashActivity.kt
@@ -12,15 +12,22 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>() {
+class SplashActivity :
+    BaseActivity<ActivitySplashBinding, SplashViewModel>() {
 
-    override fun getViewBinding(): ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+    override fun getViewBinding(): ActivitySplashBinding =
+        ActivitySplashBinding.inflate(layoutInflater)
+
     override val viewModel: SplashViewModel by viewModels()
 
     override fun setupUI() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, HomeActivity::class.java))
-            finish()
-        }, 2000)
+        if (viewModel.hasConnection) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+            }, 2000)
+        } else {
+            //todo show internet connection error
+        }
     }
 }
--- app/src/main/java/com/celiluysal/itunesexplorer/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/celiluysal/itunesexplorer/ui/splash/SplashViewModel.kt
@@ -1,9 +1,13 @@
 package com.celiluysal.itunesexplorer.ui.splash
 
 import androidx.lifecycle.ViewModel
+import com.celiluysal.itunesexplorer.utils.NetworkUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor() : ViewModel() {
+class SplashViewModel @Inject constructor(private val networkUtil: NetworkUtil) : ViewModel() {
+
+    val hasConnection: Boolean = networkUtil.isNetworkAvailable()
+
 }
